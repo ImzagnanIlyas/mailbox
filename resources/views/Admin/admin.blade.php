c7602206138f15dbc6882ca6ceed2a7862ff0ed1
ImzagnanIlyas <imzagnan.ii@gmail.com>
2020-06-13 17:47:09 +0100

    fix Vuetify css

--- resources/views/Admin/admin.blade.php
+++ resources/views/Admin/admin.blade.php
@@ -21,6 +21,7 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/vuetify@2.x/dist/vuetify.min.css" rel="stylesheet">
 </head>
 <body>
     <div id="app">
